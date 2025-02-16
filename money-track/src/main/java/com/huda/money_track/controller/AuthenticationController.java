c2849d8876996af1430386290fe6b0dfbd83a150
O2F-IT\huudang.nguyen_amari <dang.nguyen.con@beintl.com>
2025-02-16 13:12:20 +0700

    16/2/2025: Reformat AuthenticationController.java file

--- money-track/src/main/java/com/huda/money_track/controller/AuthenticationController.java
+++ money-track/src/main/java/com/huda/money_track/controller/AuthenticationController.java
@@ -9,11 +9,17 @@ import com.huda.money_track.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
